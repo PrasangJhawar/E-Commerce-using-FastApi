83c952c6856cf8ed348774bc1faae425ed052f8d
Prasang <prasangjhawar@gmail.com>
2025-06-12 18:46:13 +0530

    added orders/models

---
+++ app/orders/models.py
@@ -0,0 +1,33 @@
+from sqlalchemy import Column, ForeignKey, Integer, String, Float, DateTime
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
+from app.core.database import Base
+import uuid
+from datetime import datetime
+
+
+#used for one particular order
+class Order(Base):
+    __tablename__ = "orders"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    user_id = Column(UUID(as_uuid=True), ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    total_amount = Column(Float, nullable=False)
+    status = Column(String, default="processing")
+
+    items = relationship("OrderItem", back_populates="order", cascade="all, delete")
+
+
+#used to track each item of the order(more like real world)
+class OrderItem(Base):
+    __tablename__ = "order_items"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    order_id = Column(UUID(as_uuid=True), ForeignKey("orders.id", ondelete="CASCADE"), nullable=False)
+    product_id = Column(UUID(as_uuid=True), ForeignKey("products.id", ondelete="CASCADE"), nullable=False)
+    quantity = Column(Integer, nullable=False)
+    price = Column(Float, nullable=False)
+
+    order = relationship("Order", back_populates="items")
+    product = relationship("Product")
